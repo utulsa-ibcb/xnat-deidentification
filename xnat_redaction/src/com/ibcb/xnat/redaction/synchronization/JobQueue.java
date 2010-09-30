ecb47baee795f400172adc7914245ccb45800e39
Matt Matlock <mmatlock@xnat.ibcb.utulsa.edu>
2010-09-30 15:13:53 -0700

    This is empty.

--- xnat_redaction/src/com/ibcb/xnat/redaction/synchronization/JobQueue.java
+++ xnat_redaction/src/com/ibcb/xnat/redaction/synchronization/JobQueue.java
@@ -11,7 +11,7 @@ public class JobQueue {
 	
 	static JobQueue singleton = null;
 	
-	public static class Status{
+	private static class Status{
 		String message;
 		int state;
 		int percent;
@@ -57,8 +57,9 @@ public class JobQueue {
 		
 		public String getMessage(String task){
 			if(job_status.containsKey(task)){
-				
+				return job_status.get(task).message;
 			}
+			return null;
 		}
 		
 	}
@@ -68,8 +69,30 @@ public class JobQueue {
 		return singleton;
 	}
 	
+	public HashMap<Long, Job> jobs_by_id;
+	public HashMap<String, Job> jobs_by_name;
+	
+	public JobQueue(){
+		jobs_by_id = new HashMap<Long, Job>();
+		jobs_by_name = new HashMap<String, Job>();
+	}
 	
 	public long	createJob(String name){
-		
+		Job j = new Job(name);
+		jobs_by_id.put(j.job_id, j);
+		jobs_by_name.put(name, j);
+		return j.job_id;
+	}
+	
+	public Job getJob(long id){
+		if(jobs_by_id.containsKey(id))
+			return jobs_by_id.get(id);
+		return null;
+	}
+	
+	public Job getJob(String name){
+		if(jobs_by_name.containsKey(name))
+			return jobs_by_name.get(name);
+		return null;
 	}
 }
