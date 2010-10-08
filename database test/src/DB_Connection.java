6cc3f7d071985d7480862172d940068ede16cc9e
xinchi <xinchi@xinchi-laptop.(none)>
2010-10-07 23:48:02 -0500

    update

--- database test/src/DB_Connection.java
+++ database test/src/DB_Connection.java
@@ -20,11 +20,89 @@ public class DB_Connection {
 	}
 	public void createTable(Connection con,String table_name)
 	{
-		 
+		 int res = 1;
+		 try {
+			PreparedStatement pstat = con.prepareStatement("SELECT COUNT (relname) FROM pg_class WHERE relname = '"+table_name+"';");
+			ResultSet rs = pstat.executeQuery();
+			while(rs.next())
+			{
+				res = rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(res==0)
+		{
+			System.out.println("TABLE DOES NOT EXIT, WILL CREATE ONE");
+			String cmd ="CREATE TABLE "+table_name+"(SubjectID varchar(80), FakePHIData text,RequestID varchar(80),ProjectID varchar(80) );";
+			try{
+				Statement stat=con.createStatement();
+				stat.execute(cmd);
+			}catch(SQLException e)
+			{
+				System.err.print(e);
+			}
+			
+		}
+		else
+		{
+			System.out.println("TABLE ALREADY EXIST!!");
+		}
 	}
-	public void query()
+	public void query_print(String var_name,String table_name,Connection con)
 	{
-		
+		try{
+		PreparedStatement pstat = con.prepareStatement("select "+var_name+" from "+table_name+";");
+		ResultSet rs = pstat.executeQuery();
+		while(rs.next())
+		{
+			
+			System.out.println(rs.getString("subjectid"));
+		}
+		}catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public ResultSet query(String var_name,String table_name,Connection con)
+	{
+		PreparedStatement pstat;
+		ResultSet rs =null;
+		try {
+			pstat = con.prepareStatement("select "+var_name+" from "+table_name+";");
+			rs = pstat.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rs;
+	}
+	public int add(Connection con,String sql)
+	{
+		int result=0;
+		try {
+			Statement stat = con.createStatement();
+			
+			result = stat.executeUpdate(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+	public int update(Connection con,String sql)
+	{
+		int result=0;
+		try {
+			Statement stat = con.createStatement();
+			
+			result = stat.executeUpdate(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 }
