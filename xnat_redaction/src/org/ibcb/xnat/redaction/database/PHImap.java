8a4ee166fc728a1442b1655dab30cdebc603498a
liang kong <kong@kong-laptop.(none)>
2011-01-18 20:19:39 -0600

    Add PHI map Change database structure

---
+++ xnat_redaction/src/org/ibcb/xnat/redaction/database/PHImap.java
@@ -0,0 +1,17 @@
+package org.ibcb.xnat.redaction.database;
+
+public class PHImap {
+	public String getPHIname(int phi_id)
+	{
+		return null;
+	}
+	public int getPHIId(String PHI)
+	{
+		
+		return null;
+		
+	}
+	
+	
+
+}
