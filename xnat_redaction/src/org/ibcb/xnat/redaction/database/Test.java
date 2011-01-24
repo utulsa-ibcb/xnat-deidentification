3e7e185daab0c92b6218c96f482e29067ef14060
liang kong <kong@kong-laptop.(none)>
2011-01-23 19:19:43 -0600

    daily update

--- xnat_redaction/src/org/ibcb/xnat/redaction/database/Test.java
+++ xnat_redaction/src/org/ibcb/xnat/redaction/database/Test.java
@@ -13,8 +13,9 @@ public class Test {
 		// TODO Auto-generated method stub
 
 		//SubjectInfo info = new SubjectInfo("sub_test3","phi_test3","req_test3","proj_test3");
-		//RequestInfo rinfo = new RequestInfo("req_test3","usr_test3","2010-10-28","adm_test3","{test!!!,test!!!,test!!!,test!!!}");
-		//DBManager db1 = new DBManager(rinfo,DBManager.INSERT_REQUESTINFO);
+//Problem....
+		RequestInfo rinfo = new RequestInfo("req_test3","usr_test3","2010-10-28","adm_test3",new java.sql.Array(new int[]{1,2,3,4}));
+		DBManager db1 = new DBManager(rinfo,DBManager.INSERT_REQUESTINFO);
 		//DBManager db = new DBManager(info,DBManager.INSERT_SUBJECTINFO);
 		//DBManager db2 = new DBManager(DBManager.QUERY_REQUESTINFO);
 		//DBManager db3 = new DBManager(DBManager.QUERY_SUBJECTINFO);
@@ -22,7 +23,7 @@ public class Test {
 		
 		
 		//new Thread(db).run();
-		//new Thread(db1).run();
+		new Thread(db1).run();
 		//new Thread(db2).run();
 		//new Thread(db3).run();
 		//new Thread(united).run();
