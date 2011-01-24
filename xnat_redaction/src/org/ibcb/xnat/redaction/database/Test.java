a74b957ad1f3a613feb172ad2e7ea87dd60ae4fb
xinchi <xinchi@xinchi-laptop.(none)>
2011-01-24 10:07:32 -0600

    stephen's version of binary replacement with KMP algorithm

--- xnat_redaction/src/org/ibcb/xnat/redaction/database/Test.java
+++ xnat_redaction/src/org/ibcb/xnat/redaction/database/Test.java
@@ -2,6 +2,7 @@ package org.ibcb.xnat.redaction.database;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Array;
 
 
 public class Test {
@@ -11,10 +12,9 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		//SubjectInfo info = new SubjectInfo("sub_test3","phi_test3","req_test3","proj_test3");
 //Problem....
-		RequestInfo rinfo = new RequestInfo("req_test3","usr_test3","2010-10-28","adm_test3",new java.sql.Array(new int[]{1,2,3,4}));
+		RequestInfo rinfo = new RequestInfo("req_test3","usr_test3","2011-01-23","adm_test3",new int[]{1,2,3,4});
 		DBManager db1 = new DBManager(rinfo,DBManager.INSERT_REQUESTINFO);
 		//DBManager db = new DBManager(info,DBManager.INSERT_SUBJECTINFO);
 		//DBManager db2 = new DBManager(DBManager.QUERY_REQUESTINFO);
@@ -31,9 +31,9 @@ public class Test {
 		
 		//DBManager update1 = new DBManager(DBManager.UPDATE_SUBJECTINFO,DBManager.FAKEPHIDATA,"phi_test11",DBManager.PROJECTID,"sub_test1");
 		//new Thread(update1).run();
-		DBManager update2 = new DBManager(DBManager.UPDATE_REQUESTINFO,DBManager.CHECKOUTINFO,"{update,update,update,update}",DBManager.ADMINID,"adm_test1");
+		//DBManager update2 = new DBManager(DBManager.UPDATE_REQUESTINFO,DBManager.CHECKOUTINFO,"{update,update,update,update}",DBManager.ADMINID,"adm_test1");
 			
-		new Thread(update2).run();
+		//new Thread(update2).run();
 	}
 
 }
--- xnat_redaction/src/org/ibcb/xnat/redaction/helpers/BinaryReplacement.java
+++ xnat_redaction/src/org/ibcb/xnat/redaction/helpers/BinaryReplacement.java
@@ -9,57 +9,87 @@ import java.util.List;
 public class BinaryReplacement {
 	
 	public static void main(String[] args) throws IOException{
-//		byte z = (byte)'0';
-//		byte one = (byte)'1';
-//		byte[] find = {z, one, one, z};
-//		byte[] r = {one, z, z, one};
-//		File file = new File("whatever.txt");
-//		LinkedList<byte[]> l = new LinkedList<byte[]>();
-//		l.add(find);utulsa
-//		find_and_replace(file, l, r);
-		byte zero =(byte)'0';
-		byte one = (byte)'1';
-		byte[] r = {one,one,one};
 		File file = new File("whatever.txt");
-		LinkedList<Integer> offset = new LinkedList<Integer>();
-		offset.add(30);
-		replace(file,offset,r);
+        byte[] rep = byteArray("yak");
+		LinkedList<byte[]> patts = new LinkedList<byte[]>();
+		patts.add(byteArray("ame"));
+		patts.add(byteArray("eis"));
+		patts.add(byteArray("ply"));
+		patts.add(byteArray("lyt"));
+		find_and_replace(file, patts, rep);
 	}
+
+    public static byte[] byteArray(String s) {
+        char[] carr = s.toCharArray();
+        byte[] barr = new byte[carr.length];
+        for (int i = 0; i < carr.length; i++) barr[i] = (byte)carr[i];
+        return barr;
+    }
+
+    //Calculates prefix functions (also known as overlap functions) for the modified KMP matcher
+    public static int[][] prefixFuncs(List<byte[]> patterns, int m) {
+        int i = 0;
+        int[][] pfuncs = new int[patterns.size()][m];
+        for (byte[] patt : patterns) {
+            int[] pi = pfuncs[i];
+            pi[0] = -1;
+            int k = -1;
+            for (int q = 1; q < m; q++) {
+                while (k >= 0 && patt[k+1] != patt[q]) k = pi[k];
+                if (patt[k+1] == patt[q]) k++;
+                pi[q] = k;
+            }
+            i++;
+        }
+        return pfuncs;
+    }
 	
-	public static void find_and_replace(File file, List<byte[]> bytes, byte[] replace) throws IOException
-	{
-		int size = bytes.size();
-		if (size == 0) return;
-		
-		int len = bytes.get(0).length;
-		int rlen = Math.min(len, replace.length); //Ideally, len and replace.length (the bits to be replaced and replacing bits, respectively)
-		                                          //will match, but if not, we can only use the smaller of the two for replacements.
-		byte[] b = new byte[len];
-				
-		RandomAccessFile rw = null;
-		try {
-			rw = new RandomAccessFile(file, "rw");
-			while (rw.read(b) == len) {
-				
-				label:
-				for (byte[] arr : bytes) {
-					for (int i = 0; i < len; i++) {
-						if (arr[i] != b[i]) continue label; //If we don't have a match, go on to the next byte array to replace.
-					}
-                    long fp = rw.getFilePointer();
-					rw.seek(fp - len); //Since testing for a match moved the file pointer to the end of where we need to replace, we move it back.
-					rw.write(replace, 0, rlen); //Write rlen bytes to the file at offset 0 from the current position.
-					rw.seek(fp + len); //Writing moved the file pointer rlen bytes forward. We want to move it len bytes forward.
-				}
-			}
-		}
+    //This method assumes that everything in List<byte[]> bytes
+    //and that byte[] replace are all of the same lengths.
+    //It uses the KMP string searching algorithm to find matches.
+	public static void find_and_replace(File file, List<byte[]> bytes, byte[] replace) throws IOException {
+        int size = bytes.size();
+        if (size == 0) return;
+
+        int len = replace.length;
+
+        //Modified KMP (Knuth-Morris-Pratt) Matcher
+        int[][] pfuncs = prefixFuncs(bytes, len);
+        int[] matchindex = new int[size];
+        for (int i = 0; i < size; i++) matchindex[i] = -1;
+
+        int b;
+        RandomAccessFile rw = null;
+        try {
+            rw = new RandomAccessFile(file, "rw");
+            long fp = 0; //File pointer
+            while ((b = rw.read()) != -1) {
+                fp++; //We read a byte, so advance the file pointer
+                int i = 0;
+                for (byte[] patt : bytes) {
+                    int[] pi = pfuncs[i]; //Get the prefix function corresonding to the current pattern
+                    int q = matchindex[i]; //Number of bytes currently matching the pattern minus one
+                    while (q >= 0 && patt[q+1] != b) q = pi[q];
+                    if (patt[q+1] == b) q++;
+                    if (q == len-1) { //We found a match
+                        rw.seek(fp-len);
+                        rw.write(replace);
+                        //Since we replaced the matched patterns, we reset
+                        //the number the number of bytes matching each pattern
+                        for (int j = 0; j < size; j++) matchindex[j] = -1; 
+                        break;                                             
+                    }
+                    matchindex[i] = q; //Write the index of the last element of the longest matching prefix back to the array
+                    i++; //Increment the loop counter
+                }
+            }
+        }
 		finally {
 			if (rw != null) rw.close();
 		}
 	}
 
-	public static void replace(File file, List<Integer> offset, byte[] replace) throws IOException
-	{
+	public static void replace(File file, List<Integer> offset, byte[] replace) throws IOException {
 		if(offset.size() == 0) return;
 		RandomAccessFile rw = null;
 
@@ -78,5 +108,5 @@ public class BinaryReplacement {
 			if (rw!=null) rw.close();
 		}
 	}
-	
+
 }
